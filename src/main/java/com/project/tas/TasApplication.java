0e74187411a40de56e623bbcd13495f334993cc4
JeongSooNa <skwjdtn3171@naver.com>
2022-06-23 10:49:10 +0900

    feat js

---
+++ src/main/java/com/project/tas/TasApplication.java
@@ -0,0 +1,13 @@
+package com.project.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tas/TasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
