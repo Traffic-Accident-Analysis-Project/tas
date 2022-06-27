639f1998fef77cafc09152dd13e2c746ae989576
stooooopu <jyg5193@naver.com>
2022-06-27 20:27:21 +0900

    fix stopu column 수정해야함

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS tas_manager (
 	manager_id varchar(30) NOT NULL PRIMARY KEY COMMENT '관리자 ID',
 	manager_password varchar(100) COMMENT '관리자 비밀번호'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- 고객센터 Table
 CREATE TABLE IF NOT EXISTS tas_board (
 	board_no int AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT 'index',
@@ -20,4 +21,15 @@ CREATE TABLE IF NOT EXISTS tas_board (
 	board_cnt int default 0 COMMENT '조회수',
 	board_comment varchar(500) COMMENT '답변',
 	board_is_comment boolean default false COMMENT '코멘트 여부'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- user Table
+CREATE TABLE IF NOT EXISTS tas_user( -- user 테이블
+	id INTEGER(4) AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '회원 번호',
+	user_id VARCHAR(12) NOT NULL UNIQUE COMMENT '회원 아이디',
+	user_name VARCHAR(10) NOT NULL COMMENT '회원 이름',
+	user_password VARCHAR(200) NOT NULL COMMENT '회원 비밀번호',
+	user_gender VARCHAR(2) NOT NULL COMMENT '회원 성별',
+	user_addr VARCHAR(200) NOT NULL COMMENT '회원 주소',
+	autority VARCHAR(10) NOT NULL COMMENT '권한' -- 그럼 회원가입할 때 이걸 어떻게 보여주지????
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
