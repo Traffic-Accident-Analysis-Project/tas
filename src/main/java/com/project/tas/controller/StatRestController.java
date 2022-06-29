12401fa5e89006ff9c0b51096a2d062e51774d92
JeongSooNa <skwjdtn3171@naver.com>
2022-06-29 15:59:10 +0900

    feat js

--- src/main/java/com/project/tas/controller/StatRestController.java
+++ src/main/java/com/project/tas/controller/StatRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,26 +18,31 @@ public class StatRestController {
 	private StatService statService;
 	
 	// 법규위반별 Data
+	@CrossOrigin
 	@GetMapping("/law")
 	public List<Map<String, Object>> callAllLawData() {
 		return statService.getAllLawData();
 	}
 	// 사고유형별 Data
+	@CrossOrigin
 	@GetMapping("/accident")
 	public List<Map<String, Object>> callAllAccidentData() {
 		return statService.getAllAccidentData();
 	}
 	// 지역별 Data
+	@CrossOrigin
 	@GetMapping("/city")
 	public List<Map<String, Object>> callAllCityData() {
 		return statService.getAllCityData();
 	}
 	// 도로별/요일별 Data
+	@CrossOrigin
 	@GetMapping("/road")
 	public List<Map<String, Object>> callAllRoadData() {
 		return statService.getAllRoadData();
 	}
 	// 음주측정별 Data
+	@CrossOrigin
 	@GetMapping("/alcohol")
 	public List<Map<String, Object>> callAllAlcoholData() {
 		return statService.getAllAlcoholData();
