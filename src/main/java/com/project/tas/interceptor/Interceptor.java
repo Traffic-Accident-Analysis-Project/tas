c142cdbc032f384b0352eb88cbec77f6c1ae4e62
JeongSooNa <skwjdtn3171@naver.com>
2022-07-05 12:36:03 +0900

    feat js

--- src/main/java/com/project/tas/interceptor/Interceptor.java
+++ src/main/java/com/project/tas/interceptor/Interceptor.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,35 +21,42 @@ public class Interceptor implements HandlerInterceptor {
 
 	@Autowired
 	private LogsService logsService;
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		String url = request.getRequestURI();
 		String ip = request.getHeader("X-forwarded-For");
 		String httpMethod = request.getRemoteAddr();
-		
-		if(ip == null) {
+
+		if (ip == null) {
 			ip = request.getRemoteAddr();
 		}
-		
-		System.out.println("ip : "+ip);
-		System.out.println("url :  "+url);
-		System.out.println("httpMethod : "+httpMethod);
-		
+
+		System.out.println("ip : " + ip);
+		System.out.println("url :  " + url);
+		System.out.println("httpMethod : " + httpMethod);
+
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
 		String time = formatter.format(Calendar.getInstance().getTime());
-		System.out.println("time : "+time);
-		
+		System.out.println("time : " + time);
+
 		LogsVO vo = new LogsVO();
 		vo.setUrl(url);
 		vo.setIp(ip);
 		vo.setHttpMethod(httpMethod);
 		vo.setCreateAt(time);
-		
+
 		logsService.setLogs(vo);
-		
+
+		// session check (isLogin)
+		HttpSession session = request.getSession();
+		if (session.getAttribute("userId") == null) {
+			// session 값이 없으면 다시 요청
+			response.sendRedirect("/login");
+		}
+
 		return true;
 	}
 
