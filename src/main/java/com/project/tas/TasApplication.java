592b1ea14b661c8a612fa8a93de9e295b03cc437
stooooopu <jyg5193@naver.com>
2022-07-13 08:44:20 +0900

    feat js

--- src/main/java/com/project/tas/TasApplication.java
+++ src/main/java/com/project/tas/TasApplication.java
@@ -2,13 +2,20 @@ package com.project.tas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasApplication {
+public class TasApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasApplication.class);
+	}
+	
 }
